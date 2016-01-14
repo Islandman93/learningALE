86f1bdcfbc81e8d246ad7ab94c461a2ed1ef9167
Islandman93 <islandman93@gmail.com>
2016-01-13 19:29:02 -0500

    DQN and nn optimizations

--- learningALE/learners/DQN.py
+++ learningALE/learners/DQN.py
@@ -13,29 +13,25 @@ class DQNLearner(learner):
         rand_vals = (1, 0.1, 1000000)  # starting at 1 anneal eGreedy policy to 0.1 over 1,000,000/skip_frame
         self.action_handler = ActionHandler(ActionPolicy.eGreedy, rand_vals)
 
-        self.exp_handler = ExperienceHandler(1000000/skip_frame)
-        self.train_handler = TrainHandler(32, num_actions)
-        self.cnn = CNN((None, skip_frame, 86, 80), num_actions, 1)
+        self.exp_handler = ExperienceHandler(1000000/skip_frame, replay_start_size=100)
+        self.cnn = CNN((None, skip_frame, 86, 80), num_actions)
 
-        self.discount = .99
+        self.discount = .95
+        self.costList = list()
 
         if load is not None:
             self.cnn.load(load)
-            self.action_handler = ActionHandler(ActionPolicy.eGreedy, (0.05, 0.05, 2))
-            self.exp_handler = ExperienceHandler(1000000/skip_frame)
-            class dummy():
-                def train_qlearn(self, a, b, c):
-                    return
-            self.train_handler = dummy()
 
     def frames_processed(self, frames, action_performed, reward):
         self.exp_handler.add_experience(frames, self.action_handler.game_action_to_action_ind(action_performed), reward)
-        self.train_handler.train_qlearn(self.exp_handler.get_random_experience, self.get_est_reward, self.cnn)
-        self.action_handler.anneal()
+        # train cnn
+        # generate minibatch data
+        states, actions, rewards, state_tp1s, terminal, inds = self.exp_handler.get_random_experience(32)
 
-    def get_est_reward(self, state_tp1s, terminal):
-        r_tp1 = self.cnn.get_output(state_tp1s)
-        return (1-terminal) * self.discount * np.max(r_tp1, axis=1)
+        if states is not None:
+            cost = self.cnn.train(states, actions, rewards, state_tp1s, terminal)
+            self.costList.append(cost)
+        self.action_handler.anneal()
 
     def get_action(self, game_input):
         return self.cnn.get_output(game_input)[0]
@@ -54,5 +50,26 @@ class DQNLearner(learner):
         self.cnn.save(file)
 
     def get_cost_list(self):
-        return self.train_handler.costList
+        return self.costList
+
+
+class DQNTester:
+    def __init__(self, skip_frame, num_actions, load, rand_val=0.05):
+        rand_vals = (rand_val, rand_val, 2)
+        self.action_handler = ActionHandler(ActionPolicy.eGreedy, rand_vals)
+        self.cnn = CNN((None, skip_frame, 86, 80), num_actions, 1)
+        self.cnn.load(load)
+        self.q_vals = list()
+
+    def get_game_action(self, game_input):
+        q_vals = self.cnn.get_output(game_input)[0]
+        self.q_vals.append(q_vals)
+        return self.action_handler.action_vect_to_game_action(q_vals)
+
+    def frames_processed(self, a, b, c):
+        pass
+
+    def set_legal_actions(self, legal_actions):
+        self.action_handler.set_legal_actions(legal_actions)
+
 
